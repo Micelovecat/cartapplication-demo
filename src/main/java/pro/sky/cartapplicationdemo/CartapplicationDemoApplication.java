510ab3f274d802d28ff8fb7800970faa9c7369fd
Micelovecat <flora_bux@mail.ru>
2022-03-14 21:47:30 +0300

    Initial commit

---
+++ src/main/java/pro/sky/cartapplicationdemo/CartapplicationDemoApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.cartapplicationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartapplicationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartapplicationDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/cartapplicationdemo/CartapplicationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.cartapplicationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartapplicationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
