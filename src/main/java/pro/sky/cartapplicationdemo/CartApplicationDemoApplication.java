23f84e4cbcd3b3bb520ba926dd58c535aefa7e84
Micelovecat <flora_bux@mail.ru>
2022-03-14 21:52:47 +0300

    First commit

--- src/main/java/pro/sky/cartapplicationdemo/CartApplicationDemoApplication.java
+++ src/main/java/pro/sky/cartapplicationdemo/CartApplicationDemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CartapplicationDemoApplication {
+public class CartApplicationDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CartapplicationDemoApplication.class, args);
+		SpringApplication.run(CartApplicationDemoApplication.class, args);
 	}
 
 }
